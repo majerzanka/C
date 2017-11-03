dad000d1be3194050f4fe5c011e49c212b63eb26
Gregory Basket <33025713+majerzanka@users.noreply.github.com>
2017-11-03 17:36:24 +0100

    Update szyfrCezaraSprawdzanie.cpp

--- szyfrCezaraSprawdzanie.cpp
+++ szyfrCezaraSprawdzanie.cpp
@@ -57,13 +57,10 @@ int main() {
     for(size_t i = 0; i < 2;i++)
         for(size_t k = 0; k < 26 ; k++)
         {
-            if(i)
-            {
                 nowy_napis = szyfr(k,nieszyfr_str[i]);
                 if(nowy_napis == szyfr_str[i])
                     cout << "Napis prawidlowy ma indeks" << i << endl;
 
-            }
 
         }
 
